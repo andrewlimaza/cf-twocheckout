3f0a590ab0f01ed80475afe8ad0fc779b37631c6
Andrew Lima <andrew@arctek.co.za>
2018-08-31 08:42:05 +0200

    Checkout redirect to 2checkout fix.

--- includes/functions.php
+++ includes/functions.php
@@ -42,7 +42,7 @@ function cf_2checkout_set_transient($form, $referrer, $process_id ){
 		$transdata['twocheckout'][ 'return_url' ] = $return_url;
 	}
 
-
+	return $transdata;
 }
 
 /**
@@ -57,11 +57,9 @@ function cf_2checkout_set_transient($form, $referrer, $process_id ){
  */
 function cf_2checkout_redirect_to_checkout( $url, $form, $processid ){
 	global $transdata;
-		if( ! empty( $transdata['twocheckout' ][ $processid ][ 'url' ] ) && empty( $_GET['error'] ) && empty( $_GET['error_description'] ) ) {
-			$saved_url = $transdata['twocheckout' ][ $processid ][ 'url' ];
-			return $saved_url;
-		}
-
+	if ( ! empty( $transdata[ 'twocheckout' ] ) && ! empty( $transdata[ 'twocheckout' ][ 'url' ] ) ) {
+		return $transdata[ 'twocheckout' ][ 'url' ];
+	}
 	return $url;
 }
 
@@ -101,66 +99,7 @@ function cf_2checkout_register_processor( $processors ) {
  */
 function cf_2checkout_pre_processor( $config, $form, $proccesid ) {
 	global $transdata;
-
-	/**
-	 * Complete submission after coming back from Dwolla.
-	 */
-	if( !empty( $_GET['cf-twocheckout-payment-confirmation'] ) && '2checkout' == $_GET['cf-twocheckout-payment-confirmation'] ){
-		if ( isset( $_GET[ 'processid' ] ) && isset( $transdata[ 'twocheckout' ] ) && isset( $transdata[ 'twocheckout' ][ $_GET[ 'processid' ] ] ) ) {
-
-			/**
-			 * @var Caldera_Forms_Processor_Get_Data
-			 */
-			$processor_data = $transdata[ 'twocheckout' ][ $_GET[ 'processid' ] ][ 'process_object' ];
-			if ( ! is_object( $processor_data )  ){
-				return array(
-					'type' => 'error',
-					'note' => __( 'Error completing transaction', 'cf-twocheckout' )
-				);
-			}
-
-			if ( ! isset( $transdata[ $proccesid ][ 'meta'] ) ) {
-				$transdata[ $proccesid ][ 'meta'] = array();
-			}
-
-			$payment_data = $processor_data->get_values();
-			if ( isset( $_GET['orderId'] ) && isset( $_GET['status'] ) ) {
-				$transdata[ $proccesid ][ 'meta' ][ 'orderId' ] = $_GET['orderId'];
-				$transdata[ $proccesid ][ 'meta' ][ 'status' ] = $_GET[ 'status' ];
-
-				if ( $_GET['status'] == 'Completed' ) {
-					if ( $_GET['signature'] == hash_hmac( 'sha1', $_GET['checkoutId'] . '&' . $_GET['amount'], $payment_data['dwolla_api_secret'] ) ) {
-						$transdata[ $proccesid ][ 'meta' ][ 'transaction' ] = strip_tags( $_GET[ 'transaction' ] );
-						$transdata[ $proccesid ][ 'meta' ][ 'clearingDate' ] = strip_tags( $_GET[ 'clearingDate' ] );
-
-						return;
-
-					} else {
-						$processor_data->add_error( 'Invalid Signature', 'cf-twocheckout' );
-					}
-				} else {
-					$processor_data->add_error( 'Transaction Failed', 'cf-twocheckout' );
-				}
-			}else{
-				$processor_data->add_error( 'Transaction Failed', 'cf-twocheckout' );
-			}
-		}
-
-		if( ! empty( $_GET['error'] ) && ! empty( $_GET[ 'error_description' ] ) ){
-			$processor_data->add_error( urldecode( $_GET['error_description' ] ) );
-		}
-
-		//If we have errors, report them and bail
-		$errors = $processor_data->get_errors();
-		if ( ! empty( $errors  ) ) {
-			return $errors;
-
-		}
-
-		return;
-
-	}
-
+		
 	/**
 	 * New Submission
 	 */
@@ -174,28 +113,37 @@ function cf_2checkout_pre_processor( $config, $form, $proccesid ) {
 		return $errors;
 
 	}
-	//record data for this proccessor for saving
-	$transdata[ $proccesid ] = $processor_data->get_values();
 
-	$processor_data = cf_2checkout_process_payment(  $processor_data, $proccesid );
+	$payment_data = $processor_data->get_values();
+	
+	// Set URL.
+	$url = 'https://2checkout.com/checkout/purchase';
 
-
-	$errors = $processor_data->get_errors();
-
-	if ( ! empty( $errors  ) ) {
-		return $errors;
+	if ( isset( $payment_data['sandbox'] ) ) {
+		$url = 'https://sandbox.2checkout.com/checkout/purchase';
 	}
 
-	if( isset($transdata['twocheckout' ][ $proccesid ][ 'url' ] ) ){
-		// set transient expire to longer to allow user a longer login setup etc..
-		$transdata['expire'] = 1800; // 30 min should give enough time to register if needed.
-		return array(
-			'type' => 'redirect',
-			'url' => $transdata['twocheckout' ][ $proccesid ][ 'url' ]
-		);
+	$args = array(
+		"sid" 					=> $payment_data['account_number'],
+		"mode" 					=> "2CO",
+		"li_0_type"				=> "product",
+		"li_0_name"				=> $payment_data['item_name'],
+		"li_0_quantity"			=> $payment_data['item_quantity'],
+		"li_0_price"			=> $payment_data['price'],
+		"li_0_product_id"		=> '1',
+		"li_0_tangible"			=> 'N',
+		"pay_method"			=> 'CC',
+		"currency_code"			=> $payment_data['currency'],
+	);
+	
+	// Add code here for recurring.
 
-	}
+	$url = add_query_arg( $args, $url );
 
+	$transdata[ 'twocheckout' ][ 'url' ] = $url;
+	return array(
+		'type' => 'success'
+	);
 }
 
 /**
@@ -219,106 +167,6 @@ function cf_2checkout_process( $config, $form, $proccessid ) {
 
 	return $transdata[ $proccessid ][ 'meta' ];
 
-}
-
-/**
- * Process payment and prepare the URL to redirect to Dwolla.
- *
- * @since 0.1.0
- *
- * @param Caldera_Forms_Processor_Get_Data $processor_data
- *
- * @return Caldera_Forms_Processor_Get_Data
- */
-function cf_2checkout_process_payment( $processor_data, $proccessid ) {
-	global $transdata;
-	if ( ! isset( $transdata['twocheckout'] ) || ! isset( $transdata['twocheckout'][ 'return_url' ]  ) ) {
-		$processor_data->add_error( 'Could not set redirect URL.', 'cf-twocheckout' );
-		return $processor_data;
-	}
-
-	// Set default 2Checkout URL
-	$url = 'https://www.2checkout.com/checkout/purchase';
-
-	$callback = add_query_arg(
-		array(
-			'cf-twocheckout-payment-confirmation' => '2checkout',
-			'cf_tp' => $transdata['transient'], // add in the cf_tp ( Caldera Forms Transient Process - this is a shortcut to reprocess a transient form submission - Document this please! )
-			'processid' => $proccessid,
-
-		),
-		$transdata['twocheckout'][ 'return_url' ]
-	);
-
-	if ( empty( $payment_data['orderId'] ) ) {
-		$payment_data['orderId'] = $proccessid;
-	}
-
-	$payment_data = $processor_data->get_values();
-
-	$body = array(
-		"sid" 				=> $payment_data['account_number'],
-		"mode" 				=> "2CO",
-		"li_0_type"			=> "product",
-		"li_0_name"			=> $payment_data['item_name'],
-		"li_0_quantity"		=> $payment_data['item_quantity'],
-		"li_0_price"		=> $payment_data['price'],
-		"li_0_product_id"	=> '1',
-		"li_0_tangible"		=> 'N',
-		"pay_method"		=> 'CC',
-		"purchase_step" 	=> 'billing-information',
-		"currency_code"		=> $payment_data['currency']
-		);
-
-	if( $processor_data->get_value( 'sandbox' ) ){
-		$body[ 'test' ] = true;
-		$url = 'https://sandbox.2checkout.com/checkout/purchase';
-	}else{
-		$body[ 'test' ] = false;
-	}
-
-	$body = wp_json_encode( $body );
-
- 	$result = wp_remote_request( $url, array(
-			'body' => $body,
-			'method' => 'POST',
-			'headers' => array(
-	             'content-type' =>  'application/json',
-	             'timeout' => 60
-			),
-		)
-	);
-
-	if ( is_wp_error( $result ) ) {
-		$processor_data->add_error( __( sprintf( 'Transaction Failed', 'cf-twocheckout' ) ) );
-	}else{
-
-		
-		$response_body = wp_remote_retrieve_body( $result );
-		$response_body = (array) json_decode( $response_body );
-		if (isset( $response_body[ 'Result' ] ) ) {
-
-			if ( 'Failure' == $response_body[ 'Result' ] ) {
-				if ( isset( $response_body[ 'Message' ] ) ) {
-					$processor_data->add_error( $response_body[ 'Message' ] );
-				}else{
-					$processor_data->add_error( __( sprintf( 'Transaction Failed', 'cf-twocheckout' ) ) );
-				}
-
-
-			}else {
-				if ( isset( $response_body['CheckoutId'] ) ) {
-					$url = $url . $response_body['CheckoutId'];
-
-					$transdata['twocheckout' ][ $proccessid ][ 'url' ] = $url;
-					$transdata['twocheckout' ][ $proccessid ][ 'CheckoutId' ] = $response_body['CheckoutId'];
-					$transdata['twocheckout' ][ $proccessid ][ 'process_object' ] = $processor_data;
-				}
-			}
-		}
-	}
-
-	return $processor_data;
 }
 
 /**
